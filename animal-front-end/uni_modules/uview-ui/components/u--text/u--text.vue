69639eac06d9b588394233482c8b82d1f9b5fc21
dyxin <2232591215@qq.com>
2025-12-02 23:03:51 +0800

    feat:test组件

--- animal-front-end/uni_modules/uview-ui/components/u--text/u--text.vue
+++ animal-front-end/uni_modules/uview-ui/components/u--text/u--text.vue
@@ -30,7 +30,7 @@
 /**
  * 此组件存在的理由是，在nvue下，u-text被uni-app官方占用了，u-text在nvue中相当于input组件
  * 所以在nvue下，取名为u--input，内部其实还是u-text.vue，只不过做一层中转
- * 不使用v-bind="$attrs"，而是分开独立写传参，是因为微信小程序不支持此写法
+ * 不使用v-bind="$attrs"，而是分开独立写传参，是因为微信小程序不支持此写法。
  */
 import uvText from "../u-text/u-text.vue";
 import props from "../u-text/props.js";
